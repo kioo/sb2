9d15e1092b3a19a1605f5232e636fe9d368228f0
wangchi@chinasofti.com <f2114198@163.com>
2020-07-07 14:58:53 +0800

    add timer test case

--- src/main/java/com/mooc/sb2/Sb2Application.java
+++ src/main/java/com/mooc/sb2/Sb2Application.java
@@ -4,6 +4,7 @@ import com.mooc.sb2.initializer.SecondInitializer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StopWatch;
 
 import javax.swing.*;
 
@@ -11,12 +12,26 @@ import javax.swing.*;
 @MapperScan("com.mooc.sb2.mapper")
 public class Sb2Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
-//		SpringApplication.run(Sb2Application.class, args);
-		SpringApplication springApplication = new SpringApplication(Sb2Application.class);
-		springApplication.addInitializers(new SecondInitializer());
-		springApplication.run(args);
+		SpringApplication.run(Sb2Application.class, args);
+
+		// add Initializer
+//		SpringApplication springApplication = new SpringApplication(Sb2Application.class);
+//		springApplication.addInitializers(new SecondInitializer());
+//		springApplication.run(args);
+
+		// timer use way
+//		StopWatch stopWatch = new StopWatch();
+//		stopWatch.start("task1");
+//		Thread.sleep(1000L);
+//		stopWatch.stop();
+//		stopWatch.start("task2");
+//		Thread.sleep(2000L);
+//		stopWatch.stop();
+//
+//		System.out.println(stopWatch.prettyPrint());
+
 	}
 
 }
