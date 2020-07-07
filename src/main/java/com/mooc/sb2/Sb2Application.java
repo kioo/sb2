edc088df7286e13e8ca2a01ecf5e7e924e336216
wangchi@chinasofti.com <f2114198@163.com>
2020-07-07 09:16:19 +0800

    add window client test upload

--- src/main/java/com/mooc/sb2/Sb2Application.java
+++ src/main/java/com/mooc/sb2/Sb2Application.java
@@ -12,6 +12,7 @@ import javax.swing.*;
 public class Sb2Application {
 
 	public static void main(String[] args) {
+
 //		SpringApplication.run(Sb2Application.class, args);
 		SpringApplication springApplication = new SpringApplication(Sb2Application.class);
 		springApplication.addInitializers(new SecondInitializer());
